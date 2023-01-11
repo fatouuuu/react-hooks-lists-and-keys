3b399cee4679c1d300d7102a6f992da56d27d36e
fatouuuu <fatmaabdullrahim@gmail.com>
2023-01-11 15:23:45 +0300

    completed lesson

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ColorList from "./ColorList";
 
+
+
 function App() {
   return (
     <div>
--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,12 @@
 import React from "react";
 
+function ColorItem(props) {
+  return <li style={{ color: props.color }}>{props.color}</li>;
+  
+}
+// console.log(props)
+
+// ColorList component
 function ColorList() {
   const colors = [
     "firebrick",
@@ -7,20 +14,35 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "black"
   ];
 
+  const colorElements = colors.map((color) => {
+    return <ColorItem key={color} color={color} />;
+  });
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {/* display the array of <li> elements here! */}
+        {colorElements}
       </ol>
     </div>
   );
+
+
+  // return (
+  //   <div>
+  //     <h1>Top 5 CSS Colors</h1>
+  //     <ol>
+  //       <li style={{ color: colors[0] }}>{colors[0]}</li>
+  //       <li style={{ color: colors[1] }}>{colors[1]}</li>
+  //       <li style={{ color: colors[2] }}>{colors[2]}</li>
+  //       <li style={{ color: colors[3] }}>{colors[3]}</li>
+  //       <li style={{ color: colors[4] }}>{colors[4]}</li>
+  //     </ol>
+  //   </div>
+  // );
 }
 
 export default ColorList;
